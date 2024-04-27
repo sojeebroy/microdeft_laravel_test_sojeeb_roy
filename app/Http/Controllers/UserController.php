0279b4062a66996c13ae9c508737c01a1664f8d0
taheermahmud <143108220+taheermahmud@users.noreply.github.com>
2024-04-27 13:02:14 +0600

    Route added and started crud operation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UserController extends Controller
 {
-    //
+    public function Seed()
+    {
+        return view('welcome');
+
+    }
+
+    public function Create(Request $request)
+    {
+
+    }
 }
